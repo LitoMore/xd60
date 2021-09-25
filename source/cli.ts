e3d1bb96cec87b72aad2053535de4f98c255a7e9
LitoMore <litomore@gmail.com>
2021-09-26 02:44:03 +0800

    Split code

---
+++ source/cli.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import process from 'node:process';
+import {dirname, join} from 'node:path';
+import {fileURLToPath} from 'node:url';
+import meow from 'meow';
+import ora from 'ora';
+import {reflash, autoReflash} from './utils.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const spinner = ora('');
+
+let firmwarePath = join(__dirname, '../keymaps', 'mac.hex');
+
+const cli = meow(
+	`
+  Usage:
+    $ kbd reflash
+    $ kbd reflash <file>
+    $ kbd auto
+    $ kbd auto <file>
+`,
+	{
+		importMeta: import.meta,
+	},
+);
+
+if (cli.input.length > 0) {
+	const command = cli.input[0];
+
+	if (cli.input[1]) {
+		firmwarePath = cli.input[1];
+	}
+
+	switch (command) {
+		case 'reflash':
+			await reflash(spinner, firmwarePath);
+			break;
+		case 'auto':
+			autoReflash(spinner, firmwarePath);
+			break;
+		default:
+			spinner.fail('Invalid command');
+			process.exit(1);
+	}
+} else {
+	cli.showHelp();
+	process.exit(1);
+}
---
+++ source/utils.ts
@@ -0,0 +1,50 @@
+/* eslint
+	unicorn/no-process-exit:off,
+	@typescript-eslint/no-unsafe-call: off,
+	@typescript-eslint/no-unsafe-member-access: off,
+	@typescript-eslint/no-implicit-any-catch: off */
+import process from 'node:process';
+import {Ora} from 'ora';
+import execa, {ExecaError} from 'execa';
+import usb from 'usb';
+
+export type ReflashFunc = (spinner: Ora, firmwarePath: string) => Promise<void>;
+export type AutoReflashFunc = (spinner: Ora, firmwarePath: string) => void;
+
+export const reflash: ReflashFunc = async (spinner, firmwarePath) => {
+	try {
+		spinner.start();
+		spinner.text = 'Erasing';
+		await execa('dfu-programmer', ['atmega32u4', 'erase', '--force']);
+		spinner.text = 'Flashing';
+		await execa('dfu-programmer', ['atmega32u4', 'flash', firmwarePath]);
+		spinner.text = 'Reseting';
+		// @ts-expect-error: Expected call
+		usb.removeAllListeners();
+		await execa('dfu-programmer', ['atmega32u4', 'reset']);
+		spinner.succeed('Done');
+	} catch (error: any) {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const execaError: ExecaError = error;
+		spinner.fail(execaError.stderr.trim() || execaError.message);
+		process.exit(1);
+	}
+};
+
+export const autoReflash: AutoReflashFunc = (spinner, firmwarePath) => {
+	spinner.start();
+	spinner.text = 'Waiting for XD60';
+
+	const autoClose = setTimeout(() => {
+		// @ts-expect-error: Expected call
+		usb.removeAllListeners();
+		spinner.fail('No device present');
+		process.exit(1);
+	}, 15_000);
+
+	usb.on('attach', async () => {
+		await execa('dfu-programmer', ['atmega32u4', 'get']);
+		clearTimeout(autoClose);
+		await reflash(spinner, firmwarePath);
+	});
+};
